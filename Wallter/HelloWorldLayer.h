4c34832156bd77d99129730d9162790c21a9956b
Najati <najati@gmail.com>
2012-09-24 11:00:13 -0400

    wallter

---
+++ Wallter/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Wallter
+//
+//  Created by Najati Imam on 9/24/12.
+//  Copyright Cyrus Innovation 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Wallter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Wallter
+//
+//  Created by Najati Imam on 9/24/12.
+//  Copyright Cyrus Innovation 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
