4a1bc00080fe96f032deedfb506531528bdbf5a9
Najati <najati@gmail.com>
2012-12-23 00:44:04 -0500

    better title image

--- Wallter/Application/Layers/IntroLayer.m
+++ Wallter/Application/Layers/IntroLayer.m
@@ -22,21 +22,11 @@
 	[super onEnter];
 	CGSize size = [[CCDirector sharedDirector] winSize];
 
-	CCSprite *background;
-
-	if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
-		background = [CCSprite spriteWithFile:@"instructions.png"];
-		background.scale = 2; // TODO maybe only do this on retina display
-	} else {
-		background = [CCSprite spriteWithFile:@"instructions~ipad.png"];
-	}
-	background.position = ccp(size.width / 2, size.height / 2);
+	CCSprite *background = [CCSprite spriteWithFile:@"title.jpg"];
+	background.scale = size.height / background.contentSize.height;
+	background.anchorPoint = cgp(0, 0);
 
 	[self addChild:background];
-}
-
--(void)onEnterTransitionDidFinish {
-	[self scheduleOnce:@selector(makeTransition:) delay:0.5];
 }
 
 - (void)makeTransition:(ccTime)dt {
