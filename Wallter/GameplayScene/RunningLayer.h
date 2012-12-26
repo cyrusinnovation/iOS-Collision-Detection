252e686b0e8e3325730d83210023e0d84368b567
Najati <najati@gmail.com>
2012-12-26 02:17:37 -0500

    that didn't need to be public

--- Wallter/GameplayScene/RunningLayer.h
+++ Wallter/GameplayScene/RunningLayer.h
@@ -3,15 +3,8 @@
 // copyright cyrus innovation
 //
 
-@class WalterWeapon;
-@class WalterSimulationActor;
-@class Simulation;
-@class AudioPlayer;
-
 #define INTERFACE_LAYER 100
 
 @interface RunningLayer : CCLayerColor
 + (CCScene *)scene;
-
-- (id)init:(WalterSimulationActor *)_walterActor and:(WalterWeapon *)_walterWeapon and:(Simulation *)_simulation audioPlayer:(AudioPlayer *)_audioPlayer;
 @end
